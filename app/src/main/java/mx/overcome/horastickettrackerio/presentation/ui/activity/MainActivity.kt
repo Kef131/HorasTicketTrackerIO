ceecd837765d90d9d1e5b709518b2ca05a849cc7
Kef <kevin.gdl.131@gmail.com>
2023-05-04 21:09:15 -0600

    Structure MVVM CLEAN ARCHQUITECTURE
    - design of ticket list fragment

--- app/src/main/java/mx/overcome/horastickettrackerio/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/mx/overcome/horastickettrackerio/presentation/ui/activity/MainActivity.kt
@@ -2,11 +2,23 @@ package mx.overcome.horastickettrackerio.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import mx.overcome.horastickettrackerio.R
+import mx.overcome.horastickettrackerio.presentation.ui.fragment.TicketListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.host_fragment, TicketListFragment())
+                .commit()
+        }
     }
 }
